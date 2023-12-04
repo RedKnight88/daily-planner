41b8feb78a57b7839c6ac09f8190d2d0ef948a95
Red Knight <127162163+RedKnight88@users.noreply.github.com>
2023-12-04 16:29:35 -0500

    header date, save button listener, 9-5 hour blocks

--- script.js
+++ script.js
@@ -2,13 +2,25 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+
+  var today = dayjs();
+  var saveBtnEl = $('.saveBtn');
+  
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
+  
+  saveBtnEl.on('click', saveHandler)
+
+  function saveHandler(event) {
+    event.preventDefault();
+    var parentEl = $(this).parent();
+    var parentID = parentEl.attr('id');
+    console.log(parentID);
+  }
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
@@ -20,4 +32,7 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  var currentDatEl = $("#currentDay");
+
+  currentDatEl.text(today.format('dddd, MMMM D'));
 });
